91e39b976694a0a9886da434a1dd753378076664
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-12-31 11:02:04 -0500

    Move common styles for the app from ./src/styles to ./src/containers/App/styles

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import 'styles/core.scss'
+import styles from './styles/app.scss'
 
 // NOTE: Stateless/function components *will not* hot reload!
 // react-transform *only* works on component classes.
 const App = ({ children }) => {
   return (
-    <div className='page-container'>
-      <div className='view-container'>
-        {children}
-      </div>
+    <div className={styles.app}>
+      {children}
     </div>
   )
 }
